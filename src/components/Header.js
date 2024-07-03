b5149ebb881ff246cb29cde0572185f628f7476b
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-07-03 19:13:48 +0530

    header logo added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import LogoImage from '../assets/logo.jpg';
 
 const Header = () => {
   return (
     <header className="bg-222222 text-white py-4">
       <nav className="flex justify-between items-center max-w-6xl mx-auto px-4">
-        <div className="text-2xl font-bold">Logo</div>
+        <img src={LogoImage} className="w-12 rounded"/>
         <ul className="flex space-x-4">
           <li><a href="#home" className="nav-link">Home</a></li>
           <li><a href="#projects" className="nav-link">Projects</a></li>
