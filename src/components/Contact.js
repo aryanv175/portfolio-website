7eadec01c435c68abbc7b0716a0090cf21a08478
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-07-03 18:20:50 +0530

    better

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,9 +11,9 @@ const Contact = () => {
         Contact me at: <a href="mailto:email@example.com" className="text-pink-500">email@example.com</a>
       </p>
       <div className="flex justify-center space-x-4 mt-8">
-        <a href="#" className="text-white text-2xl"><i className="fab fa-twitter"></i></a>
-        <a href="#" className="text-white text-2xl"><i className="fab fa-github"></i></a>
-        <a href="#" className="text-white text-2xl"><i className="fab fa-linkedin"></i></a>
+        <a href="https://x.com/DevWithAryan" className="text-white text-2xl"><i className="fab fa-twitter"></i></a>
+        <a href="https://github.com/aryanv175" className="text-white text-2xl"><i className="fab fa-github"></i></a>
+        <a href="https://www.linkedin.com/in/aryanv175/" className="text-white text-2xl"><i className="fab fa-linkedin"></i></a>
       </div>
     </section>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gradient-start': '#FF8660',
+        'gradient-end': '#9A33FF',
+      },
+    },
   },
   plugins: [],
-}
+};
