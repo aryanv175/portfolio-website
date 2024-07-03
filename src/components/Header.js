80580bbb32844f39728ed67f5eea7df9e4436f80
Aryan Verma <91381804+aryanv175@users.noreply.github.com>
2024-07-03 19:24:43 +0530

    logo added

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="bg-222222 text-white py-4">
       <nav className="flex justify-between items-center max-w-6xl mx-auto px-4">
-        <img src={LogoImage} className="w-12 rounded"/>
+        <img src={LogoImage} alt="Logo" className="w-12 rounded"/>
         <ul className="flex space-x-4">
           <li><a href="#home" className="nav-link">Home</a></li>
           <li><a href="#projects" className="nav-link">Projects</a></li>
